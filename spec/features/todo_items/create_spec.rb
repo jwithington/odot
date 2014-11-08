709b1293492f21d59111c6a27b9807a8f05ae954
Jim Withington <jamesrwithington@gmail.com>
2014-11-07 20:28:14 -0500

    Finished work on todo items and associated validations

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -20,4 +20,26 @@ it "is successful with valid content" do
 		expect(page).to have_content("Milk")
 		end
 	end
+
+	it "displays an error with no content" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: ""
+		click_button "Save"
+		within("div.flash") do
+			expect(page).to have_content("There was a problem adding that todo list item")
+		end
+		expect(page).to have_content("Content can't be blank")
+	end
+
+	it "displays an error with content less than 2 characters long" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: "1"
+		click_button "Save"
+		within("div.flash") do
+			expect(page).to have_content("There was a problem adding that todo list item")
+		end
+		expect(page).to have_content("Content is too short")
+	end
 end
