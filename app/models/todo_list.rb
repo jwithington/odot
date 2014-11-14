b8bed98ce4c0c7e5a9e94b9088448fe59cccd264
Jim Withington <jamesrwithington@gmail.com>
2014-11-14 14:30:43 -0500

    Prettify the views as last step in the course

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -5,4 +5,12 @@ has_many :todo_items
 	validates :title, length: { minimum: 3}
 	validates :description, presence: true
 	validates :description, length: { minimum: 5}
+	
+	def has_completed_items?
+		todo_items.complete.size > 0
+	end
+
+	def has_incomplete_items?
+		todo_items.incomplete.size > 0
+	end
 end
