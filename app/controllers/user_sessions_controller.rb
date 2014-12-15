a7d55e5f5da080084cd41bd6efad333631aba548
Jim Withington <jamesrwithington@gmail.com>
2014-12-14 16:57:20 -0800

    Adding the rest of the user_sessions files

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,16 @@
+class UserSessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by(email: params[:email])
+  	if user && user.authenticate(params[:password])
+	  	session[:user_id] = user.id
+	  	flash[:success] = "Thanks for logging in!"
+	  	redirect_to todo_lists_path
+	  else
+	  	flash[:error] = "We had a problem logging you in. Please check your email and password."
+	  	render action: 'new'
+	  end
+  end
+end
