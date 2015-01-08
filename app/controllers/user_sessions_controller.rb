e33bac08e5dfc10478201786239b27e9cf69642b
Jim Withington <jamesrwithington@gmail.com>
2015-01-07 22:11:20 -0800

    changed tests and code related to remember_me

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,6 +5,10 @@ class UserSessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+     if params[:remember_me]
+      signed_token = Rails.application.message_verifier(:remember_me).generate(user.id)
+      cookies.permanent.signed[:remember_me_token] = signed_token
+    end
       session[:user_id] = user.id
       flash[:success] = "Thanks for logging in!"
       redirect_to todo_lists_path
@@ -16,6 +20,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    cookies.delete(:remember_me_token)
     reset_session
     redirect_to root_path, notice: "You have been logged out."
   end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -44,6 +44,13 @@ describe UserSessionsController do
         post :create, email: "jason@teamtreehouse.com", password: "treehouse1"
         expect(flash[:success]).to eq("Thanks for logging in!")
       end
+
+      it "sets the remember_me_token cookie if chosen" do
+        expect(cookies).to_not have_key('remember_me_token')
+        post :create, email: "jason@treehouse.com", password: "treehouse1", remember_me: "1"
+        expect(cookies).to have_key('remember_me_token')
+        expect(cookies['remember_me_token']).to_not be_nil
+      end
     end
 
     shared_examples_for "denied login" do
@@ -100,6 +107,13 @@ describe UserSessionsController do
         session[:user_id] = 1
         delete :destroy
         expect(session[:user_id]).to be_nil
+      end
+
+      it "removes the remember_me_token cookie" do
+        cookies['remember_me_token'] = 'remembered'
+        delete :destroy
+        expect(cookies).to_not have_key('remember_me_token')
+        expect(cookies['remember_me_token']).to be_nil
       end
 
       it "resets the session" do
